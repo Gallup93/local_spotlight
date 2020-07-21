acc3c934349d0cd4e111fb0f8f67e79da82787ac
Josh Tukman <josh.tukman@gmail.com>
2020-07-21 17:01:52 -0600

    Add tests for relationships

--- spec/models/user_artist_spec.rb
+++ spec/models/user_artist_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserArtist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it {should belong_to :user}
+    it {should belong_to :artist}
+  end
 end
