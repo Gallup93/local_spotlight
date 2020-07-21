ecf8a1aa79f29b00d0bafd10f2b1cf80aa9795d2
Josh Tukman <josh.tukman@gmail.com>
2020-07-21 16:59:57 -0600

    Add artist relationships and validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ApplicationRecord
+  validates_presence_of :name,
+                        :zipcode,
+                        :spotify_id
+
+  has_many :user_artists
+  has_many :users, through: :user_artists
+   
 end
