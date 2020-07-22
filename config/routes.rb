849b4d031d14a4463db701efa771609b49f666ea
Josh Tukman <josh.tukman@gmail.com>
2020-07-21 22:33:54 -0600

    Add routes for spotify Oauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/login', to: 'auth#spotify_request'
-      #get '/auth', to: 'auth#show'
       get '/user', to: 'users#create'
     end
   end 
