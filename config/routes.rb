f76231bc6bee6f56efd62e3e88f94f802e69c18a
Josh Tukman <josh.tukman@gmail.com>
2020-07-21 22:28:51 -0600

    Add route from spotify auth api call to users controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+  namespace :api do
+    namespace :v1 do
+      get '/login', to: 'auth#spotify_request'
+      #get '/auth', to: 'auth#show'
+      get '/user', to: 'users#create'
+    end
+  end 
+ 
 end
