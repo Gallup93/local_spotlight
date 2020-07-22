35a6b1119013947b23e6bb3f40bf22e16b165d97
Jack Gould <johnhilsmangould@gmail.com>
2020-07-22 16:43:17 -0600

    add better testing - add in sad path

--- spec/features/favorites/favorites_index_spec.rb
+++ spec/features/favorites/favorites_index_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe 'user favorites page', type: :feature do
     
     before :each do
         @user1 = User.create(username: "Music McMusic", email: "music@hotmail.com", zipcode: "80210" )
+        @user2 = User.create(username: "Guy McMusic", email: "Guy!!!@hotmail.com", zipcode: "80210" )
         @artist_1 = Artist.create(name: "Ramakhandra", zipcode: "80216", spotify_id: "1Apw9xiab11PyZLo6YeUoJ", city: "Denver", state: "CO", genre: ["electronic", "psychedelic"])
         @artist_2 = Artist.create(name: "Smirk", zipcode: "80126", spotify_id: "3pwiEWINB62yhDUUODnHLj", city: "Denver", state: "CO", genre: ["instrumental", "jazz"])
         @artist_3 = Artist.create(name: "American Grandma", zipcode: "80126", spotify_id: "4XTcP25C5ZUWpwf0NYGJnn", city: "Denver", state: "CO", genre: ["mellow"])
@@ -14,7 +15,8 @@ RSpec.describe 'user favorites page', type: :feature do
     it "User favorites are displayed correctly on favorites index page" do 
         UserArtist.create(user_id: @user1.id, artist_id: @artist_1.id)
         UserArtist.create(user_id: @user1.id, artist_id: @artist_2.id)
-        UserArtist.create(user_id: @user1.id, artist_id: @artist_3.id)
+        UserArtist.create(user_id: @user2.id, artist_id: @artist_4.id)
+        UserArtist.create(user_id: @user2.id, artist_id: @artist_5.id)
 
         visit '/favorites'
         within ".favorite-bands" do 
@@ -32,5 +34,14 @@ RSpec.describe 'user favorites page', type: :feature do
             end 
             expect(page).to_not have_content(@artist_4.name)
         end 
+    end 
+
+    it "Sad path for favorites - if no artists have been favorited" do 
+        visit '/favorites'
+        
+        within ".favorite-bands" do
+            expect(page).to have_content("No Favorites... Yet")
+        end 
+
     end 
 end
