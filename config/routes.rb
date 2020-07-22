f75ff589348ffb2c6f3a72c284cf6a5453f822bf
Michael Gallup <Gallup93@icloud.com>
2020-07-22 17:20:32 -0600

    Route '/dashboard' to  users show index. Add current_user to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/login', to: 'auth#spotify_request'
       get '/user', to: 'users#create'
+      get '/dashboard', to: 'users#show'
     end
-  end 
- 
+  end
 end
