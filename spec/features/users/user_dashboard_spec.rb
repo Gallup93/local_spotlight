9cfaff5e52c2f3187dcd3803cae9ad488a5e90ab
Michael Gallup <Gallup93@icloud.com>
2020-07-22 18:10:23 -0600

    Add css div class to contain each listed favorite artist

--- spec/features/users/user_dashboard_spec.rb
+++ spec/features/users/user_dashboard_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "User dashboard", type: :feature do
   before :each do
     @artist1 = Artist.create(name: "Ramakhandra", zipcode: "80216", spotify_id: "1Apw9xiab11PyZLo6YeUoJ", city: "Denver", state: "CO", genre: ["electronic", "psychedelic"])
     @artist2 = Artist.create(name: "Smirk", zipcode: "80126", spotify_id: "3pwiEWINB62yhDUUODnHLj", city: "Denver", state: "CO", genre: ["instrumental", "jazz"])
+    @artist3 = Artist.create(name: "Décollage", zipcode: "80126", spotify_id: "0fcwfASKlWfHUqoeLZBgG3", city: "Denver", state: "CO", genre: ["psychedelic", "electronic"])
 
     @user1 = User.create(username: "Rocky McMountain", email: "LoveMusic303@aol.com", zipcode: "80128")
 
@@ -28,17 +29,31 @@ RSpec.describe "User dashboard", type: :feature do
   end
 
   it "displays users favorite artists" do
-    UserArtist.create(user_id: @user1.id, artist_id: @artist2.id)
+    UserArtist.create(user_id: @user1.id, artist_id: @artist1.id)
     visit "/api/v1/dashboard"
 
     within ".favorite-artists" do
-      expect(page).to have_link(@artist2.name)
-      expect(page).to_not have_link(@artist1.name)
+      within ".artist-#{@artist1.id}" do
+        expect(page).to have_link(@artist1.name)
+        expect(page).to_not have_link(@artist2.name)
+        expect(page).to_not have_link(@artist3.name)
+      end
+    end
+
+    UserArtist.create(user_id: @user1.id, artist_id: @artist3.id)
+    visit "/api/v1/dashboard"
+
+    within ".favorite-artists" do
+      within ".artist-#{@artist3.id}" do
+        expect(page).to have_link(@artist3.name)
+        expect(page).to_not have_link(@artist2.name)
+        expect(page).to_not have_link(@artist1.name)
+      end
 
       # artist show page doesnt exist yet...
-      
-      # click_link "#{@artist2.name}"
-      # expect(current_path).to eq("/artists/#{@artist2.id}")
+
+      # click_link "#{@artist3.name}"
+      # expect(current_path).to eq("/artists/#{@artist3.id}")
     end
   end
 end
