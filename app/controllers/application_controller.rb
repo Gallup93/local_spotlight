8b89ec1e9b0879d846b56bd4088c63448e287fd4
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-23 17:05:59 -0600

    add find_artist_by_id helper method
    Co-Authored-By: Michael Gallup <Gallup815@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :find_artist_by_id
+
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def find_artist_by_id(id)
+    Artist.find(id)
   end
 end
