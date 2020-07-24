98edb652047db3fc13a0adf62bc7b348eb9292e5
Jack Gould <johnhilsmangould@gmail.com>
2020-07-23 19:48:09 -0600

    Add description to artist table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_034849) do
+ActiveRecord::Schema.define(version: 2020_07_23_221823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_034849) do
     t.string "genre", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "user_artists", force: :cascade do |t|
