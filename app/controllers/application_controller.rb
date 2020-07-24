b3fa5a6ffd36b8ee889f2f898176a818e30e231e
Michael Gallup <Gallup93@icloud.com>
2020-07-23 20:13:41 -0600

    Fix bug (users stored in db multiple times). Redirect new users to preferences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+  def return_user_by_username(username)
+    User.find_by(username: username)
+  end
 end
