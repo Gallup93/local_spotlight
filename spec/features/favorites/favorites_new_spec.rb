ce5514734f8230f7623f33301908599c20f8dc29
Michael Gallup <Gallup93@icloud.com>
2020-07-25 13:18:15 -0600

    Refactor FAV link to only show if not already a favorite

--- spec/features/favorites/favorites_new_spec.rb
+++ spec/features/favorites/favorites_new_spec.rb
@@ -31,5 +31,24 @@ RSpec.describe "Users can add a favorite" do
       expect(page).to have_content(@artist1.name)
       expect(page).to have_content(@artist3.name)
     end
+    it "wont display 'FAV' link if artist is already facortied" do
+      @user1 = User.create(username: "Music McMusic", email: "music@hotmail.com", zipcode: "80210" )
+      @artist1 = Artist.create(name: "Colfax Speed Queen", zipcode: "80126", spotify_id: "3p9nYbFprckRkRxuCFVQcx", city: "Denver", state: "CO", genre: ["garage", "punk"])
+      @artist2 = Artist.create(name: "Joel Ansett", zipcode: "80126", spotify_id: "49IjdVEbQcukWy36sdRMzl", city: "Denver", state: "CO", genre: ["indie", "pop"])
+      @artist3 = Artist.create(name: "YaSi", zipcode: "80126", spotify_id: "7emRqFqumIU39rRPvK3lbE", city: "Denver", state: "CO", genre: ["pop"])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
+      visit "/artists"
+
+      within ".fav-link" do
+        click_link "FAV"
+      end
+
+      expect(current_path).to eq("/artists")
+      expect(page).to have_content("#{@artist1.name} has been added to your favorites")
+      expect(page).to_not have_link("FAV")
+      click_link "#{@artist3.name}"
+      expect(page).to have_link("FAV")
+    end
   end
 end
