abd68f1ebde04390e2a8f8b4f3e0813de6564be5
Josh Tukman <josh.tukman@gmail.com>
2020-07-26 18:56:12 -0600

    Add helper method to get local artists to index view page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :find_artist_by_id
+  helper_method :artists_nearby
 
+  def artists_nearby(zipcodes)
+    z = zipcodes.env.response_body
+    z = z.scan(/...../)
+    acc = []
+    z.each do |zip|
+      acc << Artist.where(zipcode: zip)
+    end
+    acc2 = acc.flatten.uniq
+    acc2
+   
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
