52af1d651ffec3d742b3d3c2600585cb131be73f
Josh Tukman <josh.tukman@gmail.com>
2020-07-26 18:57:49 -0600

    Create zipcode_microservice that makes a call to outside ruby microservice file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   has_many :user_artists
   has_many :artists, through: :user_artists
 
-  def find_closest_artists
-    @artists = Artist.all
-    # MapService.new(current_user.zipcode)
+  def find_zipcodes(zip)
+    conn = Faraday.new("http://localhost:4567")
+    response = conn.get("/zipradius?radius=15&zip=#{zip}")
+  
   end
 
   def favorited?(user_id, artist_id)
