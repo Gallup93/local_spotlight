919c2e62ff0ce87e8a59284615261680c32d6879
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-29 15:30:51 -0600

    attempt at testing Spotify Oauth

--- spec/features/welcome/user_can_login_with_spotify_spec.rb
+++ spec/features/welcome/user_can_login_with_spotify_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   describe "welcome page" do
     it "has a link to login with Spotify using Oauth" do
-      
       visit "/"
 
       expect(page).to have_link('Login with Spotify')
-      #click_link "Login with Spotify"
-      #expect(current_path).to eq('/login')
+
+      expect {click_link("Login with Spotify")}.to raise_error(ActionController::RoutingError)
     end
   end
 end
